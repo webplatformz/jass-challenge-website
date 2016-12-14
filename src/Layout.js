4751b48112530c0a24e10978358c8a4379078e83
Fueglistaler <Silas.Fueglistaler@zuehlke.com>
2016-12-14 12:49:51 +0100

    renamed AppNavigation to AppNavbar.

--- src/Layout.js
+++ src/Layout.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Footer from './Footer';
-import AppNavigation from './AppNavigation';
+import AppNavbar from './AppNavbar';
 
 class Layout extends Component {
   render() {
     return (
       <div>
-        <AppNavigation />
+        <AppNavbar />
         {this.props.children}
         <Footer />
       </div>
